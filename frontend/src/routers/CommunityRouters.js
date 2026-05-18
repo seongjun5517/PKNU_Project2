fa6f996a2124a49f62e7c078c4526f6223dbe134
jason <qkr8tjdwns@naver.com>
2026-05-18 09:36:12 +0900

    tnwjd

---
+++ frontend/src/routers/CommunityRouters.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {Routes,Route} from "react-router-dom";
+
+import CommunityListPaging from "../pages/community/CommunityListPaging";
+import CommunityViewPage from "../pages/community/CommunityViewPage";
+import CommunityInsertPage from "../pages/community/CommunityInsertPage";
+
+function CommunityRouters(){
+
+    return(
+
+        <Routes>
+
+            {/* 게시판 목록 */}
+            <Route path="/community/list_Paging"
+                   element={<CommunityListPaging />}
+            />
+
+            {/* 게시글 상세 */}
+            <Route path="/community/view/:com_id" 
+                   element={<CommunityViewPage />}
+            />
+
+            {/* 게시글 등록 */}
+            <Route path="/community/insert"
+                   element={<CommunityInsertPage />}
+            />
+
+        </Routes>
+    );
+}
+
+export default CommunityRouters;
