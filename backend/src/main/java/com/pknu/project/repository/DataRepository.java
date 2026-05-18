cf559aa047cfcf43d47f2e869e9e7069f55d6497
jason <qkr8tjdwns@naver.com>
2026-05-18 09:38:16 +0900

    tnwjd

--- backend/src/main/java/com/pknu/project/repository/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pknu.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pknu.project.model.Data;
-
-public interface DataRepository extends JpaRepository<Data, String> {
-
-}
