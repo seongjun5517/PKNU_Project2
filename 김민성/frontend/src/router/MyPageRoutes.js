d9bbfb29c75ebbd3a150c796b0bcf86ee9a9dc18
jason <qkr8tjdwns@naver.com>
2026-05-19 18:06:21 +0900

    33

---
+++ 김민성/frontend/src/router/MyPageRoutes.js
@@ -0,0 +1,15 @@
+import {Routes, Route } from 'react-router-dom';
+
+import PrivateRoute from '../pages/member/LoginPage';
+import MyPage from '../pages/member/MyPage';
+
+// 마이페이지는 로그인한 사용자만 접근 가능
+function MyPageRoutes() {
+  return (
+    <Routes>
+      <Route path="/mypage" element={<PrivateRoute><MyPage /></PrivateRoute>}/>
+    </Routes>
+  );
+}
+
+export default MyPageRoutes;
