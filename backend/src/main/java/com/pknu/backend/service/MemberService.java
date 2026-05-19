ee2bceb69b97822e8ff46304218b252cca7d999b
gusrl4727 <gusrl4727@gmail.com>
2026-05-19 17:30:33 +0900

    수정 메서드 백엔드

--- backend/src/main/java/com/pknu/backend/service/MemberService.java
+++ backend/src/main/java/com/pknu/backend/service/MemberService.java
@@ -98,8 +98,7 @@ public class MemberService {
 
             Member member_update = member.get();
             member_update.setMem_name(p_member.getMem_name());
-            // this.memberRepository.save(member_update);
-
+            member_update.setMem_id(p_member.getMem_id());
             member_update.setMem_phone(p_member.getMem_phone());
 
             // return "회원 아이디 ["+p_member.getMem_id()+"]에 대한 정보가 수정되었습니다.";
