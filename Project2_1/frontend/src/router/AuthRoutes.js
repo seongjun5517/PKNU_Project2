f166b7556a8c44bbb73b7f67e894441dd1d90481
minjae <shintra0070@gmail.com>
2026-05-20 15:34:16 +0900

    파일 업로드 완료됨

--- Project2_1/frontend/src/router/AuthRoutes.js
+++ Project2_1/frontend/src/router/AuthRoutes.js
@@ -1,7 +1,6 @@
 import {Routes, Route } from 'react-router-dom';
 
 import LoginPage from '../pages/member/LoginPage';
-import FindPasswordPage from '../pages/member/FindPasswordPage';
 import GoogleAuthPage from '../pages/member/GoogleAuthPage';
 import SignupPage from '../pages/member/SignupPage';
 import MemberInsertPage from '../pages/member/MemberInsertPage';
@@ -11,9 +10,9 @@ function AuthRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/find-password" element={<FindPasswordPage />} />
       <Route path="/google-auth" element={<GoogleAuthPage />} />
       <Route path="/signup" element={<MemberInsertPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
